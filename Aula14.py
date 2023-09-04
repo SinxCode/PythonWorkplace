7b7fe9e76598f0931daa116399aa74795b593148
Sinx <gabriel.jbrito2@gmail.com>
2023-09-04 08:41:58 -0300

    FOR

---
+++ Aula14.py
@@ -0,0 +1,13 @@
+'''c = 1
+while c <10:
+    print(c)
+    c = c+1
+print('Fim')'''
+
+r = 'S'
+while r =='S':
+    n = int(input('Digite um valor: '))
+    r = str(input('Quer continuar? [S/N] ')).upper()
+print('Fim')
+
+
