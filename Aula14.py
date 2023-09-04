cdf80c2959ceb9f3eb59cdf29157fda039903ff5
Sinx <gabriel.jbrito2@gmail.com>
2023-09-04 08:55:13 -0300

    While

--- Aula14.py
+++ Aula14.py
@@ -4,10 +4,24 @@ while c <10:
     c = c+1
 print('Fim')'''
 
-r = 'S'
+'''r = 'S'
 while r =='S':
     n = int(input('Digite um valor: '))
     r = str(input('Quer continuar? [S/N] ')).upper()
-print('Fim')
+print('Fim')'''
+
+n = 1
+par = 0
+impar = 0
+while n !=0:
+    n=int(input('Digite um valor: '))
+    if n !=0:
+        if n % 2 ==0:
+            par = par+1
+        else:
+            impar = impar+1
+
+print('Voce digitou {} numeros pares e {} numeros impares! '.format(par, impar))
+
 
 
