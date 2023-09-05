2aa352f8923e0b38f6a553c225d7b149079433dd
Sinx <gabriel.jbrito2@gmail.com>
2023-09-05 09:17:48 -0300

    Tratando vários valores while

---
+++ EX064.py
@@ -0,0 +1,12 @@
+c = 0
+n = 0
+t = 0
+while n != 999:
+    n = int(input('Digite um número: '))
+    if n !=999:
+        t = t + n
+    if n != 999:
+        c = c+1
+print('Fim')
+print(c)
+print(t)    
