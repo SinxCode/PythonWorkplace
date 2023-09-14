919cce00673b91c67be2ee16d1c0d772d85246d6
Sinx <gabriel.jbrito2@gmail.com>
2023-09-14 13:28:07 -0300

    LISTA COM PARES E IMPARES

--- EX085.py
+++ EX085.py
@@ -9,7 +9,7 @@ for c in range (0,7):
 valores[0].sort() #Ordena em ordem crescente apenas os dados do indice 0
 valores[1].sort() #Ordena em ordem crescente apenas os dados do indice 1
 print(f'Os valores pares digitados foram: {valores[0]}')
-print(f'Os valores ímapres digitados foram: {valores[1]}')
+print(f'Os valores ímpares digitados foram: {valores[1]}')
 
 
     
