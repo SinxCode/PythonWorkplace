dc777314cefeb32db4913f9e866c3544055f54d4
Sinx <gabriel.jbrito2@gmail.com>
2023-09-14 09:01:17 -0300

    LISTA COMPOSTA EXEMPLO 1

---
+++ Aula 18a.py
@@ -0,0 +1,9 @@
+galera = [['João', 19], ['Ana', 33], ['Joaquim', 13], ['Maria', 45]]
+print(galera[0][0]) #traz o primeiro indice da lista galera, e o primeiro indice do conteúdo dentro do msm.
+print(galera[2][1]) #trará o indice 2 da lista galera e o conteúdo do índice 1 dentro do indíce 2 :)
+
+for p in galera: #Para cada pessoa 'p' dentro de galera, trás os dados
+    print(p)
+
+for p in galera:
+    print(p[0]) #Trará para cada pessoa dentro de galera o indice 0
