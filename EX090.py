f67f9e0c4e0390d058a4076e2417f73d9990f6d1
Sinx <gabriel.jbrito2@gmail.com>
2023-09-18 14:41:03 -0300

    Dicionario em python

--- EX090.py
+++ EX090.py
@@ -5,6 +5,8 @@ if media['Media'] < 7.5:
     media['Situação'] = 'Reprovado'
 else:
     media['Situação'] = 'Aprovado'
-print(f'O nome é igual a {media["Nome"]}')
-print(f'A média é igual a {media["Media"]}')
-print(f'A situação é igual a {media["Situação"]}')
+#print(f'O nome é igual a {media["Nome"]}')
+#print(f'A média é igual a {media["Media"]}')
+#print(f'A situação é igual a {media["Situação"]}')
+for k, v in media.items():
+    print(f'{k} é igual a {v}' ) #Este for faz exatamente o mesmo que as 3 últimas linhas, para cada key e valor dentro dos itens de media, print
