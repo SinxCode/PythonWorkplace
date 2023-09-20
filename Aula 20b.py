dc506affa50743cf92ffe22b66aa08121342d10f
Sinx <gabriel.jbrito2@gmail.com>
2023-09-20 11:43:07 -0300

    DESEMPACOTAMENTO NA FUNÇÃO

--- Aula 20b.py
+++ Aula 20b.py
@@ -8,3 +8,14 @@ def contador(*n): #O '*' informa ao python que pode ser passado vários parâmet
 contador(2, 1, 7)
 contador(8, 8)
 contador(4, 4, 7, 6, 2)
+
+
+def soma(*valores):
+    s = 0
+    for n in valores:
+        s = s + n
+    print(f'Somando os valores {valores} temos {s}')
+
+
+soma(5,2)
+soma(2,9,4) #Pela função 'soma' estar recebendo '*' valores é possível colocar vários valores dentro do parametro
