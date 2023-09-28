bd6c8ea59a5b15fb228e5f5b6d92436f93bb79d4
Sinx <gabriel.jbrito2@gmail.com>
2023-09-28 09:33:34 -0300

    EXECUTANDO MODULOS

---
+++ EX107.py
@@ -0,0 +1,7 @@
+import moeda
+
+p = float(input('Digite o preço R$: '))
+print(f'A metade de {p} é {moeda.metade(p)}')
+print(f'o dobro de {p} é {moeda.dobro(p)}')
+print(f'Aumentando em 15%, temos {moeda.aumentar(p,15)}')
+print(f'Reduzindo em 20%, temos {moeda.diminuir(p,20)}')
---
+++ moeda.py
@@ -0,0 +1,13 @@
+def metade(n):
+    return n / 2
+
+
+def dobro(n):
+    return n * 2
+
+def aumentar(n, x):
+    return n + (n * (x/100))
+
+
+def diminuir(n, x):
+    return n - (n * (x/100)) 
